c8085674c26e58ec5de5d502882a6eacdd86efe9
Armando Beltran <beltran.armando2210@gmail.com>
2023-06-24 10:48:13 -0600

    Ejemplo de listview y custom adapter

--- app/src/main/java/com/example/myfirstactivity/MyFirstActivity.kt
+++ app/src/main/java/com/example/myfirstactivity/MyFirstActivity.kt
@@ -1,24 +1,54 @@
 package com.example.myfirstactivity
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
 import android.widget.TextView
 import android.widget.Toast
+import android.widget.BaseAdapter
 
 class MyFirstActivity: Activity() {
     lateinit var contador : TextView
     lateinit var btnContador : Button
+    lateinit var myListView: ListView
     var i = 0;
+
+    var arrayList : ArrayList<MyData> = ArrayList()
+    var adapter : MyAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState) //En cada estado que ejecutemos, se tiene que poner este super obligatoriamente
 
         setContentView(R.layout.activity_my_first) //Enlaza nuestra vista (layout xml). R = recursos, layout = carpeta
-        btnContador = findViewById(R.id.btnContador)
-        contador = findViewById(R.id.contador)
+        //btnContador = findViewById(R.id.btnContador)
+        //contador = findViewById(R.id.contador)
+
+        /**val arrayAdapter: ArrayAdapter<*>
+        val games = arrayOf(
+            "Mario", "Pacman", "Zelda", "Donkey Kong",
+            "Sonic", "Crash"
+        )
+
+
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, games)
+        myListView.adapter = arrayAdapter**/
+        myListView = findViewById<ListView>(R.id.list_view)
+        arrayList.add(MyData(1, "Sonic", 9.5f))
+        arrayList.add(MyData(2, "Mario", 10.0f))
+        arrayList.add(MyData(3, "Pacman", 7.8f))
+        adapter = MyAdapter(this, arrayList)
+        myListView.adapter = adapter
+
+
 
     }
 
@@ -33,11 +63,47 @@ class MyFirstActivity: Activity() {
 
         //val intent = Intent(this, MySecondActivity::class.java)
         //startActivity(intent
-        btnContador.setOnClickListener { addNumber() }
+        //btnContador.setOnClickListener { addNumber() }
     }
 
     fun addNumber(){
         i++;
         contador.setText(i.toString())
     }
+
 }
+
+class MyAdapter(private val context: Context, private val arrayList: java.util.ArrayList<MyData>): BaseAdapter() {
+    lateinit var id : TextView
+    lateinit var name : TextView
+    lateinit var rating : TextView
+
+    override fun getCount(): Int{
+        return arrayList.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return position
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var convertView = convertView
+        convertView = LayoutInflater.from(context).inflate(R.layout.item_list, parent, false)
+        id = convertView.findViewById(R.id.id)
+        name = convertView.findViewById(R.id.gameName)
+        rating = convertView.findViewById(R.id.gameRating)
+
+        id.text = " " + arrayList[position].id
+        name.text = arrayList[position].name
+        rating.text = " " + arrayList[position].rating
+        return convertView
+    }
+
+}
+
+
+class MyData(var id: Int, var name: String, var rating: Float)
